b5a5b3eb6d76acd02c6e3e92cfc535ab8435a9e7
aaronksaunders <aaron@clearlyinnovative.com>
2015-12-08 22:39:54 -0500

    login and meteor integration completed

--- www/js/app.js
+++ www/js/app.js
@@ -3,9 +3,9 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('starter', ['ionic'])
+angular.module('starter', ['angular-meteor', 'ionic'])
 
-.run(function($ionicPlatform) {
+.run(function($ionicPlatform, $rootScope, $state) {
     $ionicPlatform.ready(function() {
       if (window.cordova && window.cordova.plugins.Keyboard) {
         // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
@@ -21,23 +21,40 @@ angular.module('starter', ['ionic'])
         StatusBar.styleDefault();
       }
     });
+
+    // checking for errors in state change
+    $rootScope.$on('$stateChangeError',
+      function(event, toState, toParams, fromState, fromParams, error) {
+        // We can catch the error thrown when the $requireUser promise is rejected
+        // and redirect the user back to the login
+        if (error === 'AUTH_REQUIRED') {
+          event.preventDefault();
+          console.log("no user");
+          $state.go('login');
+        }
+      });
   })
   .config(function($stateProvider, $urlRouterProvider) {
 
-      // For any unmatched url, send to /route1
-      $urlRouterProvider.otherwise("/login");
+    // For any unmatched url, send to /route1
+    $urlRouterProvider.otherwise("/home");
 
-      $stateProvider
-        .state('login', {
-          url: '/login',
-          // loaded into ui-view of parent's template
-          templateUrl: 'views/login.html',
-          controller : 'LoginCtrl'
-        })
-        .state('home', {
-          url: '/home',
-          // loaded into ui-view of parent's template
-          templateUrl: 'views/home.html',
-          controller: 'HomeCtrl'
-        })
-    });
+    $stateProvider
+      .state('login', {
+        url: '/login',
+        // loaded into ui-view of parent's template
+        templateUrl: 'views/login.html',
+        controller: 'LoginCtrl'
+      })
+      .state('home', {
+        url: '/home',
+        // loaded into ui-view of parent's template
+        templateUrl: 'views/home.html',
+        controller: 'HomeCtrl',
+        resolve: {
+          "currentUser": function($meteor) {
+            return $meteor.requireUser();
+          }
+        }
+      })
+  });
--- www/js/controllers.js
+++ www/js/controllers.js
@@ -3,29 +3,55 @@ angular
   .controller('LoginCtrl', LoginCtrl)
   .controller('HomeCtrl', HomeCtrl);
 
-function LoginCtrl($scope, $state) {
+function LoginCtrl($scope, $state, $meteor) {
 
-  $scope.doLoginAction = function($scope) {
-    alert("in doLoginAction");
-    setTimeout(function() {
-      $state.go('home');
-    }, 1000);
+  LoginCtrl.$inject = ['$scope', '$state', '$meteor'];
+
+  $scope.credentials = {};
+
+  $scope.doLoginAction = function() {
+    $meteor.loginWithPassword($scope.credentials.username, $scope.credentials.password)
+      .then(function() {
+        console.log('Login success ');
+        alert("logged in: " + $scope.credentials.username);
+        $state.go('home');
+      }, function(_error) {
+        console.log('Login error - ', _error);
+        alert("Error: " + _error.reason);
+      });
+    return false;
   }
 
   $scope.doCreateAccountAction = function() {
     alert("in doCreateAccountAction");
-    setTimeout(function() {
+    $meteor.createUser({
+      username: $scope.credentials.username,
+      email: $scope.credentials.username,
+      password: $scope.credentials.password,
+      profile: {
+        createdOn: new Date()
+      }
+    }).then(function(_response) {
+      console.log('doCreateAccountAction success');
+      alert("user created: " + $scope.credentials.username);
       $state.go('home');
-    }, 1000);
+    }, function(_error) {
+      console.log('Login error - ', _error);
+      alert("Error: " + _error.reason);
+    });
+    return false;
   }
 }
 
-function HomeCtrl($scope, $state) {
+function HomeCtrl($scope, $state, $meteor) {
+
+  HomeCtrl.$inject = ['$scope', '$state', '$meteor'];
+
   $scope.doLogoutAction = function() {
     alert("in doLogoutAction");
-    setTimeout(function() {
+    $meteor.logout().then(function(_response) {
       $state.go('login');
-    }, 1000);
-  }
+    });
+  };
 
 }
